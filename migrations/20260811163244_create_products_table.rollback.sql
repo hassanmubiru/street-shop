5fd4b738604e7bde9dfb5c60959d8b0fdf242b5d
hassanmubiru <hassanmubiru7@gmail.com>
2026-08-11 16:37:20 +0300

    new file:   migrations/20260811163244_create_products_table.rollback.sql 	new file:   migrations/20260811163244_create_products_table.sql

---
+++ migrations/20260811163244_create_products_table.rollback.sql
@@ -0,0 +1,8 @@
+-- Rollback: create_products_table
+-- Created: 2026-08-11T13:32:44.802Z
+-- Description: Rollback create_products_table
+
+-- Write your rollback SQL here.
+-- Example:
+--   DROP TABLE IF EXISTS create_products_table;
+
---
+++ migrations/20260811163244_create_products_table.sql
@@ -0,0 +1,12 @@
+-- Migration: create_products_table
+-- Created: 2026-08-11T13:32:44.798Z
+-- Description: 
+
+-- Write your SQL migration here.
+-- Example:
+--   CREATE TABLE create_products_table (
+--     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+--     created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+--     updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
+--   );
+
